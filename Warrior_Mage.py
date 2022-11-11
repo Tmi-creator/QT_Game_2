ebd51d072ec613087bf199866726ee2ec5e672af
Tmi-creator <74727399+Tmi-creator@users.noreply.github.com>
2022-11-11 09:22:39 +0300

    Made smth like balance

--- Warrior_Mage.py
+++ Warrior_Mage.py
@@ -7,15 +7,9 @@ class Warrior(Unit):  # armor dd
     def __init__(self):
         super().__init__(name="Warrior", hp=300, atk=30, mana=60, manacost_first=0, manacost_second=20,
                          manacost_third=60, range=1.5, move=92, first_skill_num=15,
-                         second_skill_num=20, third_skill_num=990, description_of_atk=f'atk, deals 20 damage',
-                         description_of_first='', description_of_second='', description_of_third='', picture_atk='',
-                         picture_first='', picture_second='', picture_third='')
-        # self.hp = 200
-        # self.atk = 20
-        # self.mana = 50
-        # self.first_skill_num = -10
-        # self.second_skill_num = 40
-        # self.third_skill_num = 15
+                         second_skill_num=20, third_skill_num=990, description_of_atk='atk, deals 20 damage',
+                         description_of_first='armor 15, this is simple atk', description_of_second='self.atk += 20', description_of_third='', picture_atk='',
+                         picture_first='', picture_second='', picture_third='', immortal=0)
 
     def first_skill(self, target):
         self.attack(target)
@@ -33,42 +27,18 @@ class Warrior(Unit):  # armor dd
 
 class Mage(Unit):  # super dd
     def __init__(self):
-        super().__init__(name="Mage", hp=100, atk=40, mana=100, range=3, move=2, first_skill_num=2048,
-                         second_skill_num=10, third_skill_num=15, description_of_atk=f'atk, deals 40 damage',
-                         description_of_first='', description_of_second='', description_of_third='', picture_atk='',
-                         picture_first='', picture_second='', picture_third='')
-
-    # self.hp = 100
-    # self.atk = 40
-    # self.mana = 100
-    # self.first_skill_num = 2048
-    # self.second_skill_num = 10
-    # self.third_skill_num = 15
+        super().__init__(name="Mage", hp=150, atk=40, mana=120, range=3, move=2, manacost_first=20, manacost_second=50,
+                         manacost_third=10, first_skill_num=200, second_skill_num=2, third_skill_num=20,
+                         description_of_atk='atk, deals 40 damage', description_of_first='1d4 to deal 200 damage',
+                         description_of_second='give 2 turns immortal', description_of_third='+20 mana to target',
+                         picture_atk='img/Mage.png', picture_first='img/Mage.png',
+                         picture_second='img/Mage.png', picture_third='img/Mage.png')
 
     def first_skill(self, target):
-        # def total_annihilation():
-        if self.mana >= 10:
-            target.take_damage(
-                self.first_skill_num * randint(0, 1) * randint(0, 1) * randint(0, 1) * randint(0, 1))
-            self.mana -= 10
-        else:
-            print('No mana!')
-            target.take_damage(self.atk * 0.5)
+        target.take_damage(self.first_skill_num * randint(0, 1) * randint(0, 1))
 
     def second_skill(self, target):
-        # def buff():
-        if self.mana >= 10:
-            target.atk += self.second_skill_num
-            self.mana -= 10
-        else:
-            print('No mana!')
-            target.take_damage(self.atk * 0.5)
+        target.immortal += self.second_skill_num
 
     def third_skill(self, target):
-        # def mana_up():
-        if self.mana >= 10:
-            target.mana += self.third_skill_num
-            self.mana -= 10
-        else:
-            print('No mana!')
-            target.take_damage(self.atk * 0.5)
+        target.mana += self.third_skill_num
